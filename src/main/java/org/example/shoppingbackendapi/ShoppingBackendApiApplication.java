4cd81fcb6562c3293188524588dddce77156da22
TheWaII <dhawalchudal@gmail.com>
2023-12-28 13:52:00 +0100

    init commit

---
+++ src/main/java/org/example/shoppingbackendapi/ShoppingBackendApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoppingbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/shoppingbackendapi/ShoppingBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
